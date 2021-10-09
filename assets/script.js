d87ba4b445578971298db2f3324c240ad0c78c33
Gustavo Garcia <gustavogarcia42@yahoo.com>
2021-10-09 14:26:39 -0500

    New js file created

--- assets/script.js
+++ assets/script.js
@@ -4,13 +4,10 @@ let cityWeather = $('#cityWeather');
 
 let currentWeather = `https://api.openweathermap.org/data/2.5/weather?q=${searchBar}&units=imperial&appid=b703162bc80e9c84c36e0978245d2023`;
 
-function searchWeather(searchBar) {
-    if (searchBtn.click(event)) {
+searchBtn.click(event);{
         searchBar.event.value = fetch(currentWeather)
             .then(response =>
                 response.json())
             .then(data =>
-                console.log(data));
-    }}
-
+                console.log(data));}
 
